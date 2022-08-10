eb3d9809dd8a81005cbf3b07fd7c88f1823e4d83
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-10 19:57:10 +0530

    Adding program for maximum number

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace GenericsAssignment
+{
+    public class Program
+    {
+        public static void Main(string []args)
+        {
+            Console.WriteLine("Wellcome to Generics Problem");
+
+            int output = MaximumNumber.MaximumIntegerNumber(40, 50, 60);
+            Console.WriteLine(output);
+        }
+    }
+}
+
