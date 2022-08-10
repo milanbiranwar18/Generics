d68bd30bb9ffa07709fdef9cd4ed0164e2efd76b
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-10 22:47:34 +0530

    Adding Program for Maximum Double Number

--- Program.cs
+++ Program.cs
@@ -6,8 +6,14 @@
         {
             Console.WriteLine("Wellcome to Generics Problem");
 
-            int output = MaximumNumber.MaximumIntegerNumber(40, 50, 60);
-            Console.WriteLine(output);
+            //int output = MaximumNumber.MaximumIntegerNumber(40, 50, 60);
+            //Console.WriteLine(output);
+
+            //float floatoutput = MaximumNumber.MaximumFloatsNumber(0.1, 0.2, 0.3);
+            //Console.WriteLine(floatoutput);
+
+            double doubleoutput = MaximumNumber.MaximumDoubleNumber(111.2, 22.45, 78.56);
+            Console.WriteLine(doubleoutput);
         }
     }
 }
