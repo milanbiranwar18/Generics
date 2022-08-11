2a717aa9138a7d57e46434d62cd35e8438b6ffd2
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-11 13:15:17 +0530

    Adding program for refactor all the 3to one generic methodeand finding maximum

--- Program.cs
+++ Program.cs
@@ -9,14 +9,21 @@
             //int output = MaximumNumber.MaximumIntegerNumber(40, 50, 60);
             //Console.WriteLine(output);
 
-            float floatoutput = MaximumNumber.MaximumFloatsNumber(1.6f, 2.2f, 1.3f);
-            Console.WriteLine(floatoutput);
+            //float floatoutput = MaximumNumber.MaximumFloatsNumber(1.6f, 2.2f, 1.3f);
+            //Console.WriteLine(floatoutput);
 
             //double doubleoutput = MaximumNumber.MaximumDoubleNumber(111.2, 22.45, 78.56);
             //Console.WriteLine(doubleoutput);
 
             //string strigoutput = MaximumNumber.MaximumStringNumber("Apple", "Peach", "Banana");
             //Console.WriteLine(strigoutput);
+
+           int output = GenericMaximum<int>.MaxValue(45,55,50);
+            Console.WriteLine(output);
+            float print = GenericMaximum<float>.MaxValue(4.6f, 5.3f, 6.7f);
+            Console.WriteLine(print);
+            string output1 = GenericMaximum<string>.MaxValue("Apple", "Banana", "Peach");
+            Console.WriteLine(output1);
         }
     }
 }
