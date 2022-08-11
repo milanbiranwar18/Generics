ef87f755c9e61e4f7221555b1fcffb9f52303044
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-11 09:21:15 +0530

    Adding program for maximum float numner

--- Program.cs
+++ Program.cs
@@ -9,14 +9,14 @@
             //int output = MaximumNumber.MaximumIntegerNumber(40, 50, 60);
             //Console.WriteLine(output);
 
-            //float floatoutput = MaximumNumber.MaximumFloatsNumber(0.1, 0.2, 0.3);
-            //Console.WriteLine(floatoutput);
+            float floatoutput = MaximumNumber.MaximumFloatsNumber(1.6f, 2.2f, 1.3f);
+            Console.WriteLine(floatoutput);
 
             //double doubleoutput = MaximumNumber.MaximumDoubleNumber(111.2, 22.45, 78.56);
             //Console.WriteLine(doubleoutput);
 
-            string strigoutput = MaximumNumber.MaximumStringNumber("Apple", "Peach", "Banana");
-            Console.WriteLine(strigoutput);
+            //string strigoutput = MaximumNumber.MaximumStringNumber("Apple", "Peach", "Banana");
+            //Console.WriteLine(strigoutput);
         }
     }
 }
